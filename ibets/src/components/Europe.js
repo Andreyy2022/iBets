232e271d3076a79166d5c1e3d95459d6efbf2921
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2024-02-03 15:50:41 +0300

    0.1.2

--- ibets/src/components/Europe.js
+++ ibets/src/components/Europe.js
@@ -1,6 +1,49 @@
+import { useState } from "react";
+
 function Europe() {
+    const [radioValue, setRadoiValue] = useState(1);
+
+    const text = `В Кубке европейских чемпионов учавствуют команды "Реал Мадрит" (Испания) и "Милан"(Италия). 
+        Футбольный чемпионат пройдет ${matchDate()} в Испании.`
+
+    function matchDate() {
+        const date = new Date();
+        date.setMonth(date.getMonth() + 1);
+    
+        return date.toLocaleDateString();
+    }
+
+    function changeHandler(event) {
+		setRadoiValue(event.target.value);
+	}
+	
+	const outputRadio = <div>
+		<input
+			type="radio"
+			name="radio"
+			radioValue="1"
+			checked={radioValue == '1' ? true : false}
+			onChange={changeHandler}
+		/>
+		<input
+			type="radio"
+			name="radio"
+			radioValue="2"
+			checked={radioValue == '2' ? true : false}
+			onChange={changeHandler}
+		/>
+		<input
+			type="radio"
+			name="radio"
+			radioValue="3"
+			checked={radioValue == '3' ? true : false}
+			onChange={changeHandler}
+		/>
+	</div>
+
     return <div>
-        Europe
+        <div>{text}</div>
+        <div>{outputRadio}</div>
     </div>
 }
 
